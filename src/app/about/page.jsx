9b695145247039fc6ed579c56e338155eb1bdb6c
JamersonDalvino <jamersondalvino@gmail.com>
2023-02-08 20:37:16 -0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,47 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+        doloribus natus incidunt excepturi odio iure aspernatur at. Nihil
+        laboriosam corporis sed illo soluta ut, nulla fugit neque distinctio
+        totam aliquid. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Libero, excepturi quod? Ipsum a nostrum iusto, minus quo laborum, ab
+        temporibus obcaecati odio natus architecto, tempora optio amet! Aperiam,
+        laudantium commodi! Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Eveniet, minus exercitationem quibusdam necessitatibus quisquam
+        reiciendis ullam quis nihil. Eveniet ipsa numquam minus. Nihil maxime,
+        aliquam voluptatibus doloremque impedit dignissimos neque? Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Ipsum, quisquam quisquam
+        quisquam, quisquam quisquam quisquam quisquam quisquam quisquam quisquam
+        quisquam quisquam quisquam quisquam quisquam quisquam quisquam quisquam
+        quisquam quisquam quis Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum nihil voluptates corporis suscipit non
+        dignissimos animi quas culpa architecto, eum labore amet aliquid
+        deserunt nisi, ex fuga aut aperiam. Quidem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nobis
+        sit officia? Assumenda praesentium voluptatibus pariatur nostrum, quam
+        cupiditate consequuntur velit veniam recusandae, ut necessitatibus quasi
+        accusamus quae a quas. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Minima non ratione unde et laboriosam explicabo
+        eveniet fugit aut quis alias! Suscipit hic culpa corrupti provident
+        repellendus et nostrum harum nobis. Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Accusantium, tempore. Qui perspiciatis vel
+        culpa quam est id delectus dolor quas. Tempora sint ipsa adipisci nulla
+        provident quaerat rerum modi rem.
+      </p>
+    </div>
+  )
+}
